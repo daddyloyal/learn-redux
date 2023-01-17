29ac0f9bfdef03d14f336ace809440d096c70c8c
daddyloyal <lifeofemmany6@gmail.com>
2023-01-17 13:05:44 +0100

    Done with the course, learnt reducers and combined reducers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, login } from "./actions";
+
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(login())}>Log in</button>
+      {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ''}
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { legacy_createStore as createStore} from 'redux';
-import allReducers from './reducers'
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
@@ -12,7 +13,9 @@ const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && wi
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
